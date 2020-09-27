0d975329d5d516d590e130ea53e8b2ff51aa3552
Adam Cardenas <cevaris@gmail.com>
2020-09-27 21:07:58 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// create an express app
+import express from "express"
+import { v4 as uuidV4 } from "uuid";
+
+const app = express()
+
+// define the first route
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
+
+app.get("/random/string.json", function (req, res) {
+    res.json({ value: uuidV4() });
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000,
+    () => console.log("Random API is running..."));
