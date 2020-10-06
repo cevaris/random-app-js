b5fb331717b22f9de24affe9ccab7dd5e6819ba4
Adam Cardenas <cevaris@gmail.com>
2020-10-06 02:11:57 +0000

    working dictionary api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import express from "express";
 import { v4 as uuidV4 } from "uuid";
 import * as cors from './middleware/cors';
+import { dictionary } from "./words";
 
 const app = express();
 
@@ -14,6 +15,15 @@ app.get("/", function (req, res) {
 
 app.get("/random/string.json", function (req, res) {
     res.json({ value: uuidV4() });
+})
+
+app.get("/words.json", function (req, res) {
+    if (req.query.q && typeof (req.query.q) === 'string') {
+        const query: string = req.query.q;
+        const words = dictionary.filter(w => w.word.includes(query));
+        return res.json({ words: words });
+    }
+    res.json({ message: 'missing query paramter' });
 })
 
 // start the server listening for requests
---
+++ src/words.ts
@@ -0,0 +1,35 @@
+import http from 'http';
+
+interface Word {
+    word: string
+    definition: string
+}
+
+export const dictionary: Word[] = [];
+
+if (dictionary.length === 0) {
+    http.get("http://s3.amazonaws.com/miscs.random/dictionary.sample.csv", function (response: any) {
+        let dictionaryData = '';
+        console.log(`statusCode: ${response.statusCode}`)
+
+        response.on('data', (data: string) => {
+            dictionaryData += data;
+            console.log(`got the data ${data.length}`)
+        })
+
+        response.on('end', () => {
+            console.log(`completed reading ${dictionaryData.length}`);
+            const lines = dictionaryData.split('\n');
+            lines.forEach(line => {
+                const csvData = line.split(',');
+
+                if (csvData[0] && csvData[2]) {
+                    dictionary.push({
+                        word: csvData[0].replace(/"|\r/g, ''),
+                        definition: csvData[2].replace(/"|\r/g, '')
+                    });
+                }
+            });
+        })
+    });
+}
