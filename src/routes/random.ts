9425824fcf5ea9a29cc90565ef0f72485766dde5
Adam Cardenas <cevaris@gmail.com>
2020-10-10 20:22:13 +0000

    implemented random string of length N API

--- src/routes/random.ts
+++ src/routes/random.ts
@@ -1,10 +1,27 @@
 import express from "express";
-import { v4 as uuidV4 } from "uuid";
 
 const router = express.Router();
 
-router.get("/random/string.json", function (req: express.Request, res: express.Response) {
-    res.json({ value: uuidV4() });
+interface Request extends express.Request {
+    query: {
+        length?: string
+    }
+}
+
+router.get("/random/string.json", function (req: Request, res: express.Response) {
+    const defaultLength = 10;
+    const length = parseInt(req.query.length || defaultLength.toString());
+    res.json({ value: randString(length) });
 });
 
 module.exports = router;
+
+function randString(length: number) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
