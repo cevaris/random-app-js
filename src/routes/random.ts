48e98ada98769c8e0ce2da104451c276a330f8c2
Adam Cardenas <cevaris@gmail.com>
2020-10-10 20:33:40 +0000

    working rand range

--- src/routes/random.ts
+++ src/routes/random.ts
@@ -8,6 +8,32 @@ router.get("/random/string.json", function (req: express.Request, res: express.R
     res.json({ value: randString(length) });
 });
 
+router.get("/random/number.json", function (req: express.Request, res: express.Response) {
+    if (!req.query.min) {
+        return res.status(400).json({ ok: false, message: 'missing min query parameter' });
+    }
+    if (!req.query.max) {
+        return res.status(400).json({ ok: false, message: 'missing max query parameter' });
+    }
+
+    const min = parseInt(req.query.min as string);
+    const max = parseInt(req.query.max as string);
+
+    if (isNaN(min)) {
+        return res.status(400).json({ ok: false, message: `min is not a number ${req.query.min}` });
+    }
+    if (isNaN(max)) {
+        return res.status(400).json({ ok: false, message: `max is not a number ${req.query.max}` });
+    }
+
+    if (min > max) {
+        return res.status(400).json({ ok: false, message: `min ${min} cannot be greater than max ${max}` });
+    }
+
+    const randNumber = Math.random() * (max - min) + min;
+    res.json({ value: randNumber });
+});
+
 module.exports = router;
 
 function randString(length: number) {
